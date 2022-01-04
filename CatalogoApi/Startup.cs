453e443d2add3f8db35e9f2b10ef57dd37222776
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2022-01-04 17:23:56 -0300

    Limpando a classe Startup

--- CatalogoApi/Startup.cs
+++ CatalogoApi/Startup.cs
@@ -1,17 +1,11 @@
 using CatalogoApi.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CatalogoApi
 {
@@ -24,7 +18,6 @@ namespace CatalogoApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options => 
@@ -44,7 +37,6 @@ namespace CatalogoApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
