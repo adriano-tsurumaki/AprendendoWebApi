34abe49ea3d6fd80a754e77c2e06ae47c9487787
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2022-01-04 16:49:35 -0300

    Endpoint para alterar e deletar produto

--- CatalogoApi/Controllers/ProdutosController.cs
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -40,5 +40,29 @@ namespace CatalogoApi.Controllers
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Produto produto)
+        {
+            if (id != produto.ProdutoId) return BadRequest();
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null) return NotFound();
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return produto;
+        }
     }
 }
