ab867f6ed9d3742936f245ae933ce40051b11460
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2022-01-04 10:28:17 -0300

    Adicionando o contexto das models e configurando a string de conexão com o mysql

---
+++ CatalogoApi/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+using CatalogoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        { }
+
+        public AppDbContext()
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos{ get; set; }
+    }
+}
--- CatalogoApi/Models/Produto.cs
+++ CatalogoApi/Models/Produto.cs
@@ -1,15 +1,36 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogoApi.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string Descricao { get; set; }
+
+        [Required]
         public decimal Preco { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string ImagemUrl { get; set; }
+
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
+
+
+        //Define o relacionamento
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
     }
 }
