1fcb5b37ef0e14860e80cbed775059ff284ea1d0
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2022-01-10 10:55:00 -0300

    Substituindo o retorno e a inserção nos parâmetros da model Produto para ProdutoDTO na controller de Produtos

--- CatalogoApi/Controllers/ProdutosController.cs
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using CatalogoApi.DTOs;
 using CatalogoApi.Models;
 using CatalogoApi.Repository;
 using Microsoft.AspNetCore.Http;
@@ -12,24 +14,30 @@ namespace CatalogoApi.Controllers
     public class ProdutosController : ControllerBase
     {
         private readonly IUnitOfWork _uof;
+        private readonly IMapper _mapper;
 
-        public ProdutosController(IUnitOfWork uof)
+        public ProdutosController(IUnitOfWork uof, IMapper mapper)
         {
             _uof = uof;
+            _mapper = mapper;
         }
 
         [HttpGet("menorpreco")]
-        public ActionResult<IEnumerable<Produto>> GetProdutosPrecos()
+        public ActionResult<IEnumerable<ProdutoDTO>> GetProdutosPrecos()
         {
-            return _uof.ProdutoRepository.GetProdutosPorPreco().ToList();
+            var produtos = _uof.ProdutoRepository.GetProdutosPorPreco().ToList();
+            var produtosDto = _mapper.Map<List<ProdutoDTO>>(produtos);
+            return produtosDto;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
+        public ActionResult<IEnumerable<ProdutoDTO>> Get()
         {
             try
             {
-                return _uof.ProdutoRepository.Get().ToList();
+                var produtos = _uof.ProdutoRepository.Get().ToList();
+                var produtosDto = _mapper.Map<List<ProdutoDTO>>(produtos);
+                return produtosDto;
             }
             catch
             {
@@ -39,13 +47,15 @@ namespace CatalogoApi.Controllers
         }
 
         [HttpGet("{id}", Name = "ObterProduto")]
-        public ActionResult<Produto> Get(int id)
+        public ActionResult<ProdutoDTO> Get(int id)
         {
             try
             {
                 var produto = _uof.ProdutoRepository.GetById(p => p.ProdutoId == id);
 
-                return produto is null ? NotFound($"O produto com id = {id} não foi encontrado") : produto;
+                return produto is null ? 
+                    NotFound($"O produto com id = {id} não foi encontrado") : 
+                    _mapper.Map<ProdutoDTO>(produto);
             }
             catch
             {
@@ -55,14 +65,18 @@ namespace CatalogoApi.Controllers
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] Produto produto)
+        public ActionResult Post([FromBody] ProdutoDTO produtoDto)
         {
             try
             {
+                var produto = _mapper.Map<Produto>(produtoDto);
+
                 _uof.ProdutoRepository.Add(produto);
                 _uof.Commit();
 
-                return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+                var produtoDTO = _mapper.Map<ProdutoDTO>(produto);
+
+                return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produtoDTO);
             }
             catch
             {
@@ -72,11 +86,13 @@ namespace CatalogoApi.Controllers
         }
 
         [HttpPut("{id}")]
-        public ActionResult Put(int id, [FromBody] Produto produto)
+        public ActionResult Put(int id, [FromBody] ProdutoDTO produtoDto)
         {
             try
             {
-                if (id != produto.ProdutoId) return BadRequest($"Não foi possível atualizar o produto com id = {id}");
+                if (id != produtoDto.ProdutoId) return BadRequest($"Não foi possível atualizar o produto com id = {id}");
+
+                var produto = _mapper.Map<Produto>(produtoDto);
 
                 _uof.ProdutoRepository.Update(produto);
                 _uof.Commit();
@@ -91,7 +107,7 @@ namespace CatalogoApi.Controllers
         }
 
         [HttpDelete("{id}")]
-        public ActionResult<Produto> Delete(int id)
+        public ActionResult<ProdutoDTO> Delete(int id)
         {
             try
             {
@@ -102,7 +118,7 @@ namespace CatalogoApi.Controllers
                 _uof.ProdutoRepository.Delete(produto);
                 _uof.Commit();
 
-                return produto;
+                return _mapper.Map<ProdutoDTO>(produto);
             }
             catch
             {
